5716eed7cd8f4b94fca78b00e92cffd2754e9071
Pedro Souza <pedro@pedro.com>
2022-12-07 06:33:58 -0300

    Backup

--- config/flake/.setup/hosts/notepedro/default.nix
+++ config/flake/.setup/hosts/notepedro/default.nix
@@ -4,7 +4,7 @@
   imports =                                               # For now, if applying to other system, swap files
     [(import ./hardware-configuration.nix)] ++            # Current system hardware config @ /etc/nixos/hardware-configuration.nix
     (import ../../modules/virtualization) ++
-    [import ../../modules/desktop/awesome/default.nix];   # virtualization
+    [(import ../../modules/desktop/awesome/default.nix)];   # virtualization
 
   boot = {                                  # Boot options
     kernelPackages = pkgs.linuxPackages_latest;
