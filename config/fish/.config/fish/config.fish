6fd4c8baf626c5027d1499a79886578f9bf4d9f8
Pedro Souza <pedrohms412@gmail.com>
2022-12-15 09:46:00 -0300

    Changed AppImages path location

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -16,7 +16,7 @@ set ANDROID_SDK_ROOT "$HOME/Applications/Android/Sdk"
 set ANDROID_HOME "$HOME/Applications/Android/Sdk"
 
 set -U fish_user_paths /usr/local/bin $HOME/.local/bin $HOME/Applications $fish_user_paths $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools
-set -U fish_user_paths $fish_user_paths $HOME/bin $HOME/.local/appimage
+set -U fish_user_paths $fish_user_paths $HOME/bin $HOME/Applications/AppImages
 set -U fish_user_paths $fish_user_paths $HOME/Applications/flutter/bin $HOME/Applications/genymotion
 
 ### EXPORT ###
