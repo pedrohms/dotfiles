044f0896cb3fe1eecd9a3f0bccad09133d4591c8
Pedro Souza <pedrohms412@gmail.com>
2022-12-17 13:19:37 -0300

    Some tweaks

--- notepedro/.env.notepedro.fish
+++ notepedro/.env.notepedro.fish
@@ -1,2 +1,3 @@
 alias nix-develop="nix develop -c fish"
 alias infinity="appimage-run $HOME/Applications/Appimages/InfinityWallet-2.1.5.AppImage"
+alias personal="docker run --rm -it --name teste -v personal:/root -v $PWD:/data pedrohms/gpg /bin/sh"
