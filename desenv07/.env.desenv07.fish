f7b0fa60067e5f0053ac6ea8e12ebcea2f6957e5
Pedro Souza <pedrohms412@gmail.com>
2022-12-22 20:37:38 -0300

    Added podman alias

--- desenv07/.env.desenv07.fish
+++ desenv07/.env.desenv07.fish
@@ -1,3 +1,6 @@
 set -U fish_user_paths $fish_user_paths /home/framework/Applications/android-studio/bin
 alias personal="podman run --rm -it --name teste -v personal:/root -v $PWD:/data pedrohms/gpg /bin/sh"
 alias dpersonal="docker run --rm -it --name teste -v personal:/root -v $PWD:/data pedrohms/gpg /bin/sh"
+alias mk=minikube
+alias pd=podman
+alias pdc=podman-compose
