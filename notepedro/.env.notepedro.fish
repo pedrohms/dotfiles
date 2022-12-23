4100d23fec57102f47536f61788fda8198680ae2
Pedro Souza <pedrohms412@gmail.com>
2022-12-23 09:10:01 -0300

    Added some podman alias on notepedro host

--- notepedro/.env.notepedro.fish
+++ notepedro/.env.notepedro.fish
@@ -1,3 +1,8 @@
 alias nix-develop="nix develop -c fish"
 alias infinity="appimage-run $HOME/Applications/Appimages/InfinityWallet-2.1.5.AppImage"
-alias personal="docker run --rm -it --name teste -v personal:/root -v $PWD:/data pedrohms/gpg /bin/sh"
+alias personal="podman run --rm -it --name teste -v personal:/root -v $PWD:/data pedrohms/gpg /bin/sh"
+alias dpersonal="docker run --rm -it --name teste -v personal:/root -v $PWD:/data pedrohms/gpg /bin/sh"
+alias rebuild="sudo nixos-rebuild switch --flake .#notepedro"
+alias pd=podman
+alias pdc=podman-compose
+alias dev="nix-shell -p nodejs go python3 sumneko-lua-language-server --run fish"
