de00c2a47767fb90cd7a0d9a2a219e769cde636c
Pedro Souza <pedrohms412@gmail.com>
2023-08-26 00:19:46 -0300

    Moved qtile to specific configuration for per laptop

---
+++ notepedro/.config/qtile/scripts/autostart.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+setxkbmap -layout br -variant abnt2 &
+#start sxhkd to replace Qtile native key-bindings
+run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
+conky -c $HOME/.config/conky/awesome/doom-one-01.conkyrc &
+lxsession &
+nm-applet &
+clipmenud &
+ssh-add &
+dunst &
+feh  --bg-fill $HOME/.local/wall/0001.jpg &
+picom --config $HOME/.config/picom/picom.conf --vsync &
+xset r rate 210 40 &
