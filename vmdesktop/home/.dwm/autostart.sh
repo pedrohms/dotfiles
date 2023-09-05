0125ab45931bc49399973f93dbc24c962548ce3c
Pedro Souza <pedrohms412@gmail.com>
2023-09-05 17:40:16 -0300

    Backup

--- vmdesktop/home/.dwm/autostart.sh
+++ vmdesktop/home/.dwm/autostart.sh
@@ -18,6 +18,7 @@ ssh-add &
 dunst &
 xset r rate 210 40 &
 xrandr --auto
-xrandr --output Virtual-1 --mode 1920x1080 &
+xrandr --output Virtual-1 --mode 1920x1080
 feh  --bg-fill $HOME/.local/wall/0001.jpg &
+dwmblocks &
 # picom --config $HOME/.config/picom/picom.conf --vsync &
