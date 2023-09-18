80d6edeeb82127cddce104c796358becb3e2d7ec
Pedro Souza <pedrohms412@gmail.com>
2023-09-18 11:03:01 -0300

    Separeted hyprl env.conf per device

---
+++ desenv07/home/.dwm/autostart.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+setxkbmap -layout en -variant intl &
+#start sxhkd to replace Qtile native key-bindings
+flameshot &
+sxhkd -c ~/.config/sxhkd/sxhkdrc &
+conky -c $HOME/.config/conky/awesome/doom-one-01.conkyrc &
+lxsession &
+nm-applet &
+clipmenud &
+ssh-add &
+dunst &
+xset r rate 240 40 &
+xrandr --auto
+xrandr --output HDMI-A-0 --mode 1920x1080 --rate 60 --pos 0x0 --primary --output DVI-D-0 --rate 60 --pos 1920x0
+feh  --bg-fill $HOME/.local/wall/0001.jpg &
+dwmblocks &
+# picom --config $HOME/.config/picom/picom.conf --vsync &
--- desenv07/home/.env.desenv07.fish
+++ desenv07/home/.env.desenv07.fish
@@ -3,6 +3,7 @@ alias personal="podman run --rm -it --name teste -v personal:/root -v $PWD:/data
 alias dpersonal="docker run --rm -it --name teste -v personal:/root -v $PWD:/data pedrohms/gpg /bin/sh"
 alias dev="nix develop github:pedrohms/flake-develop -c fish"
 alias dev-php="nix develop github:pedrohms/flake-develop#php -c fish"
+alias dev-jdk="nix develop github:pedrohms/flake-develop#jdk -c fish"
 alias nix-dev="nix develop -c fish"
 alias cp_flake_develop="curl -o flake.nix https://gist.githubusercontent.com/pedrohms/32bd53267885999f33f8f9b5cd60bb47/raw/04cdef82fe25569e54c3bfc7a898cf9a80097b81/flake.nix"
 alias mk=minikube
