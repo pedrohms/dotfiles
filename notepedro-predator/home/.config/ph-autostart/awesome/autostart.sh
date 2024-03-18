dd457b635fd417948ae977d35549923b8e3591d5
Pedro Souza <pedrohms412@gmail.com>
2024-03-17 22:26:29 -0300

    Backup predator host configurations

--- notepedro-predator/home/.config/ph-autostart/awesome/autostart.sh
+++ notepedro-predator/home/.config/ph-autostart/awesome/autostart.sh
@@ -8,4 +8,6 @@ function run {
 }
 # exec $HOME/.config/ph-autostart/verify_monitor.sh
 setxkbmap -layout br -variant abnt2 &
-xrandr --output eDP-1-2 --primary --mode 1920x1200 --rate 165 --pos 0x0 --output HDMI-0 --mode 1920x1080 --rate 60 --pos 1920x0
+xrandr --output HDMI-1-0 --off
+xrandr --output HDMI-1-0 --mode 1920x1080 --dpi 96 --rate 60 --pos 1920x0 --output eDP-2 --primary --mode 1920x1200 --rate 120 --pos 0x0
+# xrandr --output eDP-2 --primary --mode 1920x1200 --rate 165 --pos 0x0 --output HDMI-1-0 --mode 1920x1080 --rate 60 --pos 1920x0
--- notepedro-predator/home/.config/ph-autostart/verify_monitor.sh
+++ notepedro-predator/home/.config/ph-autostart/verify_monitor.sh
@@ -16,10 +16,10 @@ fi
 if [[ -n $IS_HDMI ]]; then
   if [[ $PH_NVIDIA -eq "2" ]]; then
     xrandr --output HDMI-1-0 --off
-    xrandr --output HDMI-1-0 --mode 1920x1080 --dpi 96 --rate 60 --pos 1920x0 --output eDP-1-1 --primary --mode 1920x1080 --rate 120 --pos 0x0
+    xrandr --output HDMI-1-0 --mode 1920x1080 --dpi 96 --rate 60 --pos 1920x0 --output eDP-2 --primary --mode 1920x1200 --rate 120 --pos 0x0
   else
     xrandr --output HDMI-0 --off
-    xrandr --output HDMI-0 --mode 1920x1080 --dpi 96 --rate 60 --pos 1920x0 --output eDP-1-1 --primary --mode 1920x1080 --rate 120 --pos 0x0
+    xrandr --output HDMI-0 --mode 1920x1080 --dpi 96 --rate 60 --pos 1920x0 --output eDP-2 --primary --mode 1920x1200 --rate 165 --pos 0x0
   fi
 else 
   if [[ -n $IS_DP_1 ]]; then
