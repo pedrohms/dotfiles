4b3e854f124da09b52de83796455c5b51597fc58
Pedro Souza <pedrohms412@gmail.com>
2025-04-25 15:48:19 -0300

    fixed some deprecated configs

--- config/nvim_lazy/.config/nvim/lua/core/plugins/gitsigns.lua
+++ config/nvim_lazy/.config/nvim/lua/core/plugins/gitsigns.lua
@@ -39,6 +39,12 @@ return {
         delay = 1000,
         ignore_whitespace = false,
       },
+<<<<<<< HEAD
+=======
+      -- current_line_blame_formatter_opts = {
+      --   relative_time = false,
+      -- },
+>>>>>>> 854cee8 (Fixed some deprecated configs)
       sign_priority = 6,
       update_debounce = 100,
       status_formatter = nil, -- Use default
@@ -51,6 +57,12 @@ return {
         row = 0,
         col = 1,
       },
+<<<<<<< HEAD
+=======
+      -- yadm = {
+      --   enable = false,
+      -- },
+>>>>>>> 854cee8 (Fixed some deprecated configs)
     }
   end
 }
